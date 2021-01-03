b65406fec64aa6d4d5317d2008755d0a25cf267c
Bryce Wong <wbryce21@gmail.com>
2021-01-02 22:40:34 -0800

    remove hash urls

--- js/global.js
+++ js/global.js
@@ -3,17 +3,17 @@ const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
 const showClass = "show";
- 
-$(window).on("load resize", function() {
+
+$(window).on("load resize", function () {
   if (this.matchMedia("(min-width: 768px)").matches) {
     $dropdown.hover(
-      function() {
+      function () {
         const $this = $(this);
         $this.addClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "true");
         $this.find($dropdownMenu).addClass(showClass);
       },
-      function() {
+      function () {
         const $this = $(this);
         $this.removeClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "false");
@@ -28,9 +28,29 @@ $(window).on("load resize", function() {
 // 60px offsert for smooth scrolling with sticky navbar
 var divId;
 
-  $('.nav-link').click(function(){    
-    divId = $(this).attr('href');
-     $('html, body').animate({
-      scrollTop: $(divId).offset().top - 60
-    }, 100);
+$('.nav-link').click(function () {
+  divId = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(divId).offset().top - 60
+  }, 100);
+});
+
+// Prevent hash urls on scroll
+$(document).ready(function () {
+
+  const menuBtn = $('.nav-link');
+
+  menuBtn.click(() => {
+    // let hash be set before removing it
+    setTimeout(() => {
+      // call removeHash function after set timeout
+      removeHash();
+    }, 5); 
   });
+
+  // removeHash function
+  // uses HTML5 history API to manipulate the location bar
+  function removeHash() {
+    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+  }
+});
