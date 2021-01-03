bbc50ff113d88b17e30b546071ee79080bce7755
Bryce Wong <wbryce21@gmail.com>
2021-01-02 22:30:40 -0800

    first commit

---
+++ js/global.js
@@ -0,0 +1,36 @@
+// Handle hover dropdown functionality for navbar
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+ 
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+// 60px offsert for smooth scrolling with sticky navbar
+var divId;
+
+  $('.nav-link').click(function(){    
+    divId = $(this).attr('href');
+     $('html, body').animate({
+      scrollTop: $(divId).offset().top - 60
+    }, 100);
+  });
